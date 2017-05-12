7ff332e4fd9c5d02f769adae0ba802688c87ef43
Ornetsmüller Raphael <a1425348@unet.univie.ac.at>
2017-05-12 20:00:17 +0200

    FR1_writeCSV

---
+++ Workbook/src/at/ac/univie/swe2/SS2017/team403/CsvWriteUtility.java
@@ -0,0 +1,29 @@
+package at.ac.univie.swe2.SS2017.team403;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CsvWriteUtility {
+	
+	public static void convertWorkSheetToCsv(Worksheet worksheet, FileWriter writer) throws IOException{
+		Integer numberOfColumns = 0; // worksheet.getHighestNumberOfColumns();
+		Integer numberOfRows = 0; // worksheet.getNumberOfRows();
+		StringBuilder cellContents = new StringBuilder();
+		
+		for(int i=0;i<numberOfRows;++i){	
+			
+			for(int j=0;j<numberOfColumns;++j){
+				cellContents.append((worksheet.getCell(i, j).toString()));
+				cellContents.append(";");
+			}
+			
+			writer.append(cellContents.toString());
+			writer.append(System.lineSeparator());
+			cellContents = new StringBuilder();
+		}
+		
+		writer.flush();
+		writer.close();
+	}	
+
+}
