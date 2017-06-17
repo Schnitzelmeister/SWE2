e1898303323eb16989b9025c887c1e91c90afa44
Tarik Pektas <a1325165@unet.univie.ac.at>
2017-06-18 00:41:39 +0200

    javadoc of the classes Billing,Customer,Invoice,Iterator

--- implementation/src/at/ac/univie/swe2/SS2017/team403/model/Customer.java
+++ implementation/src/at/ac/univie/swe2/SS2017/team403/model/Customer.java
@@ -1,7 +1,13 @@
 package at.ac.univie.swe2.SS2017.team403.model;
 
 import at.ac.univie.swe2.SS2017.team403.datagenerator.AbstractDataStorageFactory;
-
+/**
+ * 
+ * the class contains information about a customer.
+ * an object of the class will be store via factory
+ * 
+ *
+ */
 public class Customer implements Billing, CustomerReportNotifier {
 	private AbstractDataStorageFactory factory = null;
 	private String localId;
@@ -31,59 +37,110 @@ public class Customer implements Billing, CustomerReportNotifier {
 		this.phone = phone;
 	}
 	
+	/**
+	 * to set the factory which is used to store the object
+	 * @param factory
+	 */
 	public void setFactory(AbstractDataStorageFactory factory) {
 		this.factory = factory;
 	}
-
+	
+	/**
+	 * to get the local id
+	 * @return the local id of the customer
+	 */
 	public String getLocalId() {
 		return localId;
 	}
-	
+	/**
+	 * to set the local id
+	 * @param localId the local id of the customer
+	 */
 	public void setLocalId(String localId) {
 		this.localId = localId;
 	}
 
 	//a remote identifer in FastBill
+	/**
+	 * to get the remote identifer in FastBill
+	 * @return remote identifier in FastBall
+	 */
 	public String getRemoteId() {
 		return remoteId;
 	}
-
+	/**
+	 * to set the remote identifier in FastBill
+	 * @param remoteId
+	 */
 	public void setRemoteId(String remoteId) {
 		this.remoteId = remoteId;
 	}
 
+	/**
+	 * to get the last name of the customer
+	 * @return the last name
+	 */
 	public String getLastName() {
 		return lastName;
 	}
 	
+	/**
+	 * to set the last name of the customer
+	 * @param lastName the last name of the customer
+	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
+	/**
+	 * to get the first name of the customer
+	 * @return the first name of the customer
+	 */
 	public String getFirstName() {
 		return firstName;
 	}
 	
+	/**
+	 * to set the first name of the customer
+	 * @param firstName the first name of the customer
+	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
+	/**
+	 * to get the e-mail address of the customer
+	 * @return the e-mail address of the customer
+	 */
 	public String getEmail() {
 		return email;
 	}
-	
+	/**
+	 * to set the e-mail address of the customer
+	 * @param email the e-mail address of the customer
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+	/**
+	 * to get the phone number of the customer
+	 * @return the phone number of the customer
+	 */
 	public String getPhone() {
 		return phone;
 	}
-	
+	/**
+	 * to set the phone number of the customer
+	 * @param phone the phone number of the customer
+	 */
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 	
+	/**
+	 * to get an array of the subscriptions which belongs the customer
+	 * @return an array of the subscriptions
+	 */
 	public Subscription[] getSubscriptions() {
 		return factory.createSubscriptionStorage().getSubscriptionsByCustomer(this);
 	}
--- implementation/src/at/ac/univie/swe2/SS2017/team403/model/Iterator.java
+++ implementation/src/at/ac/univie/swe2/SS2017/team403/model/Iterator.java
@@ -1,5 +1,10 @@
 package at.ac.univie.swe2.SS2017.team403.model;
-
+/**
+ * 
+ * a generic iterator class
+ *
+ * @param <T> Type of an object which uses the class to iterate
+ */
 public interface Iterator<T> {
 	   public boolean hasNext();
 	   public int count();
